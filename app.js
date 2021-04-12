2e87586a11b929dd2648fbec1b3a60ccf3274954
Kossin1337 <piotrswobodzinski@gmail.com>
2021-04-12 14:48:17 +0200

    full functionality

--- app.js
+++ app.js
@@ -10,20 +10,7 @@ class Book {
 /* UI Class: Handle UI Tasks */
 class UI {
   static displayBooks() {
-    const StoredBooks = [
-      {
-        title: "Harry Potter 1",
-        author: "jk",
-        isbn: "69696969",
-      },
-      {
-        title: "Harry Potter 2",
-        author: "jk rrr",
-        isbn: "13371337",
-      },
-    ];
-
-    const books = StoredBooks;
+    const books = Store.getBooks();
 
     books.forEach((book) => UI.addBookToList(book));
   }
@@ -37,19 +24,28 @@ class UI {
     bookInfo.innerHTML = `
       <h3 class="book-info">${book.title}</h3>
       <h3 class="book-info">${book.author}</h3>
-      <h3 class="book-info">${book.isbn}</h3>
+      <h3 class="book-info book-isbn">${book.isbn}</h3>
       <button class="delete-btn"><i class="fas fa-times"></i></button>
     `;
 
     bookList.appendChild(bookInfo);
   }
 
-  static deleteBook(element) {
-    if (element.classList.contains("delete-btn")) {
-      element.parentElement.remove();
-    } else if (element.classList.contains("fa-times")) {
-      element.parentElement.parentElement.remove();
-    }
+  static deleteBook(element, isbn) {
+    element.parentElement.remove();
+    Store.removeBook(isbn);
+    UI.showAlert("Book deleted", "success");
+  }
+
+  static showAlert(message, className) {
+    const alertDiv = document.createElement("div");
+    alertDiv.className = `alert alert-${className}`;
+    alertDiv.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const form = document.querySelector(".book-form");
+    container.insertBefore(alertDiv, form);
+    // Vanish in 3 seconds
+    setTimeout(() => document.querySelector(".alert").remove(), 2000);
   }
 
   static clearFields() {
@@ -60,6 +56,37 @@ class UI {
 }
 
 /* Store Class: Handles Storage */
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook(isbn) {
+    const books = Store.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.isbn === isbn) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
 
 /* Event: Display Books */
 document.addEventListener("DOMContentLoaded", UI.displayBooks);
@@ -76,14 +103,20 @@ document.querySelector(".book-form").addEventListener("submit", (e) => {
 
   // Form Validation
   if (title === "" || author === "" || isbn === "") {
-    alert("Please fill in all fields!");
+    UI.showAlert("Please fill in all fields!", "danger");
   } else {
     // Instatiate book
     const book = new Book(title, author, isbn);
     console.log(book);
 
-    // Add Book to list
+    // Add Book to UI
     UI.addBookToList(book);
+
+    // Add Book to Store
+    Store.addBook(book);
+
+    // Show success message
+    UI.showAlert("Book added", "info");
 
     // Clear fields
     UI.clearFields();
@@ -92,5 +125,14 @@ document.querySelector(".book-form").addEventListener("submit", (e) => {
 
 /* Event: remove a book */
 document.querySelector(".books-display").addEventListener("click", (e) => {
-  UI.deleteBook(e.target);
+  if (e.target.classList.contains("delete-btn")) {
+    // console.log(`button: ${e.target.previousElementSibling.textContent}`);
+    UI.deleteBook(e.target, e.target.previousElementSibling.textContent);
+  } else if (e.target.classList.contains("fa-times")) {
+    // console.log(`i tag: ${e.target.parentElement.previousElementSibling.textContent}`);
+    UI.deleteBook(
+      e.target.parentElement,
+      e.target.parentElement.previousElementSibling.textContent
+    );
+  }
 });
---
+++ readme.md
@@ -0,0 +1,1 @@
+Book list application using vanilla Javascript with functionality allowing you to add and remove a book from your collection, form validation and custom alerts using classes and local storage
